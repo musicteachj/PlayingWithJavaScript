321a8735f80e471dd154acde63d514f32e6d2a1f
musicteachj <jimilittle3@hotmail.com>
2018-05-08 19:32:46 -0400

    first commit
    first commit in new work station

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+
+var name = prompt('What is your name?');
+var box = prompt('Which number box?');
+
+console.log(name);
+
+document.getElementById('one').innerHTML = name;
