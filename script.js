244c5855242308a371b8230924621acf0f98d3fc
musicteachj <jimilittle3@hotmail.com>
2018-05-16 19:17:23 -0400

    add all things
    added hidden class for elements, improved functionality for boxes and while loop

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+
+document.getElementById("buttonWork").addEventListener("click", function() {
+document.getElementById("buttonDiv").className = "hidden";
+document.querySelector(".jumbotron").className = "jumbotron";
+document.querySelector(".container").className = "container";
+
 var name = prompt('What is your name?');
 var box = prompt('Which number box? (1-3)');
+
+while (isNaN(box) || box >= 4 || box <= 0) {
+	var box = prompt('Please enter a number between 1-3');
+}
 
 switch (box) {
 	case "1":
@@ -16,10 +26,25 @@ switch (box) {
 	break;
 }
 
-document.getElementById("box1").className = "new";
-document.getElementById("box2").className = "new";
-document.getElementById("box3").className = "new";
 
-document.getElementById("box2").addEventListener("click", function(){
-	document.getElementById("box2").children[0].textContent = "Changed!";
+
+
+// document.getElementById("box1").className = "new";
+// document.getElementById("box2").className = "new";
+// document.getElementById("box3").className = "new";
+
+document.getElementById("box" + box).addEventListener("click", function(){
+	document.getElementById("box" + box).children[0].textContent = "Changed!";
+	document.getElementById("box" + box).className = "new";
 });
+
+var node = document.createElement("H1");                 // Create a <li> node
+var textnode = document.createTextNode("Click your name");         // Create a text node
+node.appendChild(textnode);
+node.classList.add("display-4");                              // Append the text to <li>
+document.querySelector(".jumbotron").appendChild(node);
+
+});
+
+
+
